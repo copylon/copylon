97bdda045c1ebed58634c879988c3d65093d96a0
FeedMyCat54 <konstantangelou@gmail.com>
2022-10-13 22:55:18 +0300

    Add home page screen

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:openhms/core/widgets/side_navbar.dart';
+import 'package:openhms/core/widgets/title_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,7 +10,30 @@ class HomePage extends StatelessWidget {
     final theme = Theme.of(context).colorScheme;
     return Scaffold(
       backgroundColor: theme.background,
-      body: const Center(child: Text('Home')),
+      body: Column(
+        children: [
+          const TitleBar(),
+          Expanded(
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Flexible(
+                    fit: FlexFit.loose,
+                    flex: 2,
+                    child: SideNavBar(),
+                  ),
+                  Expanded(
+                      flex: 11,
+                      child: Center(
+                        child: Text('Home'),
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
